488e99d910fc77ff77185c7a4150a87ad932633b
CarelessNick <10709148@cityplym.ac.uk>
2024-01-10 13:58:12 +0000

    Added the temperature equation to convert volts into resistance

--- Source/sensors.cpp
+++ Source/sensors.cpp
@@ -11,14 +11,28 @@ void readSensorsTask()
     DigitalOut vcc(VCC);
     DigitalOut gnd(GND);
     AnalogIn tempVolts(THERMISTOR);
+    //AnalogIn therm(THERMISTOR2);
     vcc = 1;
     gnd = 0;
     float temperatureVoltage;
-
+    
     while(true)
     {
-        temperatureVoltage = tempVolts.read();
+      temperatureVoltage = tempVolts.read();
+        float thermVolts = temperatureVoltage * 2.4; //Convert adc reading to volts
+        float current = (3.3 - thermVolts) / R_REF; //3.3V input minus the output voltage from the thermistor / by the resistor in potential divider
+        float thermResistance = (thermVolts / current); //Ohms Law to get resistance
+
+    float logrT = (float32_t)log((float64_t)thermResistance);
+        /* Calculate temperature from the resistance of thermistor using
+        * Steinhart-Hart Equation */
+        float stEqn = (float32_t)((A_COEFF) + ((B_COEFF)*logrT) +
+                                  ((C_COEFF)*pow((float64)logrT, (float32)3)));
+        float temperatureC = (float32_t)(((1.0 / stEqn) + ABSOLUTE_ZERO) + 0.05);
+
+      
         printf("The temp Voltage is: %2.2f \n", temperatureVoltage);
+        printf("The temp Temperature is: %2.2f C \n", temperatureC);
         ThisThread::sleep_for(SENSOR_RATE);
     }
 }
